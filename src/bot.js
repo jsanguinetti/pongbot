da9677ae5163715e56c4fac376e09ab8a2ef8604
jsanguinetti <jsangui@gmail.com>
2017-10-15 00:35:59 -0300

    dont 'pong' to general

--- src/bot.js
+++ src/bot.js
@@ -12,23 +12,24 @@ const config = require('./config')
 
 let bot = new RtmClient(config('SLACK_BOT_TOKEN'))
 
-let channel
+let generalChannelId
 
 // The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload
 bot.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
   let generalChannel = _.find(rtmStartData.channels, (c) => (c.is_member && c.is_general))
-  channel = generalChannel && generalChannel.id
+  generalChannelId = generalChannel && generalChannel.id
   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`)
 })
 
 // you need to wait for the client to fully connect before you can send messages
 bot.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
-  bot.sendMessage('Hello!', channel)
+  bot.sendMessage('Hello!', generalChannelId)
 })
 
 bot.on(RTM_EVENTS.MESSAGE, (message) => {
   if (message.subtype !== 'bot_message' &&
-      message.text.indexOf('/pongbot') === -1) {
+      message.text.indexOf('/pongbot') === -1 &&
+      message.channel !== generalChannelId) {
     console.log('message', message)
     bot.sendMessage('pong :table_tennis_paddle_and_ball:', message.channel)
   }
