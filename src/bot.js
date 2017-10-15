a979a94221b5a301e71d6b5c27dd6c65d869c2fc
jsanguinetti <jsangui@gmail.com>
2017-10-14 21:06:36 -0300

    demo receiving messages

--- src/bot.js
+++ src/bot.js
@@ -3,25 +3,31 @@
 
 const RtmClient = require('@slack/client').RtmClient
 const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS
 const _ = require('lodash')
 const config = require('./config')
 
 
 let bot = new RtmClient(config('SLACK_BOT_TOKEN'))
 
-let channel;
+let channel
 
 // The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload
 bot.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
   let generalChannel = _.find(rtmStartData.channels, (c) => (c.is_member && c.is_general))
   channel = generalChannel && generalChannel.id
   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
-});
+})
 
 
 // you need to wait for the client to fully connect before you can send messages
 bot.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
-  bot.sendMessage("Hello!", channel);
-});
+  bot.sendMessage('Hello!', channel);
+})
+
+bot.on(RTM_EVENTS.MESSAGE, (message) => {
+  console.log('message', message)
+  bot.sendMessage('pong :table_tennis_paddle_and_ball:', message.channel);
+})
 
 module.exports = bot
