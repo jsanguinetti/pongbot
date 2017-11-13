15197a1a1b9c3b23b70feadb81491b7e8d3cfb46
Diego Varangot <diegov@Mac-mini-de-Diego.local>
2017-11-13 16:50:26 -0300

    ranking with excel

--- src/services/rankingService.js
+++ src/services/rankingService.js
@@ -1,16 +1,51 @@
 const tableMaker = require('string-table')
 const { Ranking } = require('../models')
+const parseXlsx = require('excel');
 
 function _preformat (str) {
   return '```' + str + '```'
 }
 
 const getCurrentRanking = async () => {
-  const lastRankings = await Ranking.findAll({
-    limit: 1,
-    order: [['createdAt', 'DESC']]
-  })
-  return _preformat(tableMaker.create(lastRankings[0].table))
+  return new Promise((resolve, reject) => {
+    var rankings = []    
+    parseXlsx('Campeonato_2017.xlsx', function(err, data){
+      if (err) return reject(err)
+      let nameC = 0
+      let ptsC = 0
+      let pdsC = 0
+      let setsC = 0
+  
+      for (var i = 0; i < data[0].length; i++) {
+        var element = data[0][i];
+        if (element === "Nombre") {
+          nameC = i
+        } else if (element === "Pts") {
+          ptsC = i
+        } else if (element === "Pds") {
+          pdsC = i
+        } else if (element === "Sets") {
+          setsC = i
+        }
+      }
+  
+      for (var j = 0; j < 51; j++) {
+        let row = data[j]
+        const name = row[nameC]
+        const points = row[ptsC]
+        const matches = row[pdsC]
+        const sets = row[setsC]
+        rankings.push({
+          name: name,
+          points: points,
+          matches: matches,
+          sets: sets
+        })
+      }
+      console.log(JSON.stringify(rankings))
+      return resolve(_preformat(tableMaker.create(rankings)))
+    })
+  })  
 }
 
 module.exports = {
